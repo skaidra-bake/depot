3d7425c28360c5f469d41d3cf0852112e0bd4695
Skaidra Bake <skaidrapat@gmail.com>
2021-06-11 19:45:51 +0100

    Add extra validations and customize an error message

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, presence: true
   validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
+  validates :title, length: {
+    minimum: 10,
+    message: 'is too short. Go above 10 characters!'
+  }
+  validates :image_url, format: {
     with:     %r{\.(gif|jpg|png)\Z}i,
     message:  'must be a URL for GIF, JPG or PNG image.'
-  }
+  }, allow_blank: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
