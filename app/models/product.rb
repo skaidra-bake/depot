755bd3cf2bfa129cd52ec27e9904eb6561711b77
Skaidra Bake <skaidrapat@gmail.com>
2021-06-21 23:53:50 +0100

    Add a way to select products and add them to cart. . . Create a cart model that will be able to keep the track of the selected products during the session (through line items); create line item model that points to a product and a cart once the product is selected durning the session; create a module with a method that would set the current cart (or create a new cart if one does not previously exist); make sure the product cannot be deleted if it has been selected and put into a cart first (write a controller test just for that); Create a button that would add a line item to a cart, and once clicked will take the user to a cart (using its id) that displays a list of all line items that that are in it; style the button and a notice improve line_items controller so it would set a cart before each action and its  #create would be able to select a product and add a line item of that product to the set cart and redirect to that cart which will display a list of all the line items belonging to a cart if the action was successful. if not, it will render render #new

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, presence: true
   validates :title, uniqueness: true
   validates :title, length: {
@@ -10,4 +14,13 @@ class Product < ApplicationRecord
     message:  'must be a URL for GIF, JPG or PNG image.'
   }, allow_blank: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
