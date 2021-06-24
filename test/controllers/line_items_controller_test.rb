a218562ce9bae6bd7757c57c67182c525b0b4784
Skaidra Bake <skaidrapat@gmail.com>
2021-06-24 16:23:39 +0100

    Update a test for creating a line item

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -16,14 +16,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create line_item" do
-    assert_difference('LineItem.count') do
+      assert_difference('LineItem.count') do
       post line_items_url, params: { product_id: products(:ruby).id }
     end
 
     follow_redirect!
 
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
+    assert_select 'li', '1 × Programming Ruby 1.9'
   end
 
   test "should show line_item" do
