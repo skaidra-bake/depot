66b1b603a31801fe696728c58ca9a8fc0edeba3c
Skaidra Bake <skaidrapat@gmail.com>
2022-02-25 16:08:54 +0000

    Delete cart (only your own), show total price of items, handle recordnotfound error in the controller, redesign how the cart looks

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -62,13 +62,14 @@ class LineItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
 end
