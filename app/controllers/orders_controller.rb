d6e424e7b7760d0cb73a8350bd998c63ca737abe
Skaidra Bake <skaidrapat@gmail.com>
2022-03-25 16:42:47 +0000

    Enrure the cart isn't empty when creating new order (checkout)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,8 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: %i[ show edit update destroy ]
+  include CurrentCart
+  before_action :set_cart, only: [:new, :create]
+  before_action :ensure_cart_isnt_empty, only: :new
+  before_action :set_order, only: [ :show, :edit, :update, :destroy ]
 
   # GET /orders or /orders.json
   def index
@@ -65,5 +68,11 @@ class OrdersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
+    end
+
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: 'Your cart is empty'
+      end
     end
 end
