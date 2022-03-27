6d30264dda09811572b908d2ec2ba7ab984d049d
Skaidra Bake <skaidrapat@gmail.com>
2022-03-28 00:00:46 +0100

    Update line items price on adding an additional line item that was in the cart already

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,7 +4,11 @@ class Cart < ApplicationRecord
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
     if current_item
-      current_item.quantity += 1
+      current_item.update(
+        quantity: current_item.quantity += 1,
+        price: current_item.price + product.price
+      )
+      current_item
     else
       current_item = line_items.build(product_id: product.id, price: product.price)
     end
