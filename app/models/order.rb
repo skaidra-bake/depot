dd09525bbc00266b2851744113106812e3b645f3
Skaidra Bake <skaidrapat@gmail.com>
2022-03-28 01:17:34 +0100

    Define the relationship between order and line items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ApplicationRecord
-  validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
+  has_many :line_items, dependent: :destroy
 
   enum pay_type: {
     "Check" => 0,
     "Credit card" => 1,
     "Purchase order" => 2
   }
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
 end
